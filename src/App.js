a9e9b9f067462bb630fbdfb09d408a0dcd1d79cf
mike <mikerambil@gmail.com>
2023-05-27 00:14:52 -0500

    Added X || O"

--- src/App.js
+++ src/App.js
@@ -1,37 +1,47 @@
 import React, { useState } from "react";
 
-function Square({ value }) {
-  // eslint-disable-next-line
-  const [count, setValue] = useState(value);
-
-  function handleClick() {
-    setValue("X");
-  }
-
+function Square({ value, onSquareClick }) {
   return (
-    <button className="square" onClick={handleClick}>
-      {count}
+    <button className="square" onClick={onSquareClick}>
+      {value}
     </button>
   );
 }
 
 export default function Board() {
+  const [xIsNext, setXisNext] = useState(true);
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(index) {
+    if (squares[index]) {
+      return;
+    }
+    const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[index] = "O";
+    } else {
+      nextSquares[index] = "X";
+    }
+    setSquares(nextSquares);
+    setXisNext(!xIsNext);
+  }
+
   return (
     <React.Fragment>
       <div className="board-row">
-        <Square value={null} />
-        <Square value={null} />
-        <Square value={null} />
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
       <div className="board-row">
-        <Square value={null} />
-        <Square value={null} />
-        <Square value={null} />
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
       <div className="board-row">
-        <Square value={null} />
-        <Square value={null} />
-        <Square value={null} />
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </React.Fragment>
   );
